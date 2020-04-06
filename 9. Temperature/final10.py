6546d9f335569851bf5cd11408a80f29d0b5f56a
Nafiur Rahman <nafiur_omi@live.com>
2020-04-06 12:24:16 -0700

    Few Temperature graphs added

---
+++ 9. Temperature/final10.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import datetime 
+import matplotlib.pyplot as plt 
+import numpy as np
+
+df = pd.read_csv("uber_nyc_enriched.csv")
+
+data_to_plot = np.array([df.query('temp >= 75 and borough == ["Manhattan"]').pickups.values, df.query('temp < 75 and borough == ["Manhattan"]').pickups.values])
+
+plt.boxplot(data_to_plot)
+plt.xlabel('Temperature (Fahrenheit)')
+plt.ylabel('Pickups')
+plt.title('Temperature Vs Pickups in Manhattan')
+plt.xticks(range(1, 3, 1), ('>= 75', '<75'))
+plt.legend()
+plt.tight_layout()
+plt.show()
